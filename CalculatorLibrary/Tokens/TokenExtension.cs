91f0ed8efda4cbf0fd16474c7f00f43a9e3b3d7a
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-09-18 23:18:26 +0100

    Add stuff idk?

--- CalculatorLibrary/Tokens/TokenExtension.cs
+++ CalculatorLibrary/Tokens/TokenExtension.cs
@@ -7,5 +7,5 @@ public static class TokenExtension
     public static IToken Parse(this double num)  => new NumberToken<double>(num);
     public static void AddToStack(this IToken token) => OperationHolder.Tokens.Add(token);
     public static void Stack(this OperatorType op) => OperationHolder.Tokens.Add(new OperatorToken<OperatorType>(op));
-    public static void Stack(this double num) => OperationHolder.Tokens.Add(new NumberToken<double>(num);
+    public static void Stack(this double num) => OperationHolder.Tokens.Add(new NumberToken<double>(num));
 }
